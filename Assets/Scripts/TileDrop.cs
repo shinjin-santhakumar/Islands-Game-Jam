4835a2524852f8c5fb5578b31efd81e7c5f48570
Shinjin Santhakumar <shinjinoliver@gmail.com>
2023-06-09 00:47:43 -0700

    added grid and wave drag
    small bug but w/e

---
+++ Assets/Scripts/TileDrop.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TileDrop : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("On drop");
+
+        if (eventData.pointerDrag != null) {
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition; 
+        }
+    }
+}
